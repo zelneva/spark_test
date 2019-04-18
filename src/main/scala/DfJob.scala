a6676bb61bad7f7de3c42e24bbeb786dc8412bce
Anastasia Zelneva <anastasia.vrn97@gmail.com>
2019-04-18 16:48:08 +0300

    Calculate ratio male and female for every country

--- src/main/scala/DfJob.scala
+++ src/main/scala/DfJob.scala
@@ -26,25 +26,26 @@ object DfJob {
 
     val countryYearWindow: WindowSpec = Window.partitionBy('country).orderBy('year.desc)
     val countryPopulationWindow = Window.partitionBy('country).orderBy('populationCity.desc)
-
+    val countryWindow = Window.partitionBy('country)
 
     val countryPopulation = getPopulationForLastYears(data1, countryYearWindow)
 //    countryPopulation.rdd.saveAsTextFile(Files.outputDFPopulation)
 
     val countCitiesMillion = getCountCitiesMillon(data1, countryYearWindow)
 
-
     val top5cities = getTop5Cities(data1, countryYearWindow, countryPopulationWindow)
 
+    val ratioPopulation = getRatioMaleAndFemale(data2, countryYearWindow, countryWindow)
 
   }
 
 
-  def loadFile(path: String, sparkSession: SparkSession) = {
+  def loadFile(path: String, sparkSession: SparkSession): DataFrame = {
     sparkSession.read.format("csv").option("header", "true").load(path)
   }
 
-  def filterData(dataFrame: DataFrame) = {
+
+  def filterData(dataFrame: DataFrame): Dataset[Row] = {
     dataFrame.filter(row => row.length > 9 && row(9) != null && !row(9).toString.matches("[a-zA-Z]*"))
   }
 
@@ -58,20 +59,20 @@ object DfJob {
   }
 
 
-  def getPopulationForLastYears(data: DataFrame, maxYear: WindowSpec): DataFrame = {
+  def getPopulationForLastYears(data: DataFrame, countryYearWindow: WindowSpec): DataFrame = {
     data
       .select("country", "year", "population")
-      .withColumn("max_year", max("year").over(maxYear))
+      .withColumn("max_year", max("year").over(countryYearWindow))
       .where("year = max_year")
       .groupBy("country")
       .agg(sum("population").as("populationForLastYear"))
   }
 
 
-  def getCountCitiesMillon(data: DataFrame,  maxYear: WindowSpec): DataFrame = {
+  def getCountCitiesMillon(data: DataFrame,  countryYearWindow: WindowSpec): DataFrame = {
     data
     .select("country", "city", "year", "population")
-      .withColumn("max_year", max("year").over(maxYear))
+      .withColumn("max_year", max("year").over(countryYearWindow))
       .where("year = max_year")
       .groupBy("country", "city")
       .agg(sum("population").as("populationForLastYear"))
@@ -92,6 +93,19 @@ object DfJob {
       .orderBy( data("country"), desc("populationCity"))
       .where("row <= 5")
       .drop("row", "populationCity")
+  }
+
+
+  def getRatioMaleAndFemale(data: DataFrame,countryYearWindow: WindowSpec, countrWindow: WindowSpec ): DataFrame ={
+    data
+      .select("country", "sex", "year", "population")
+      .withColumn("max_year", max("year").over(countryYearWindow))
+      .where("year = max_year")
+      .groupBy("country", "sex")
+      .agg(sum("population").as("populationForLastYear"))
+      .withColumn("sum_pop", sum("populationForLastYear").over(countrWindow))
+      .withColumn("ratio", col("populationForLastYear").cast("double") / col("sum_pop").cast("double"))
+      .drop("populationForLastYear" , "sum_pop")
   }
 
 
